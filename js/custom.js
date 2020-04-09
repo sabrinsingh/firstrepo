632c56917cc990c330e86bf0bd0b5e0f87332e79
Sabrin Singh <sabrinlalsingh@gmail.com>
2020-04-09 12:54:42 +0545

    navbar and header padding completed

--- js/custom.js
+++ js/custom.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
                       
                     'use strict';
                       
-                    if($(window).scrollTop()<150){
+                    if($(window).scrollTop()<80){
                         
                         $('.navbar').css ({
                             
@@ -27,6 +27,11 @@ $(document).ready(function() {
                             'opacity':'1'
                             
                         });
+                        
+                        $('.navbar-nav >li > a').css(
+                        {
+                           'padding-top' : '15px' 
+                        })
                     }
    
                 });
@@ -34,7 +39,108 @@ $(document).ready(function() {
 
 
 
+$(document).ready(function(){
+    'use strict';
+    
+    
+    $('.nav-link, #scroll-to-top')
+      // Remove links that don't actually link to anything
+      .click(function(event) {
+        // On-page links
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+          && 
+          location.hostname == this.hostname
+        ) {
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000, function() {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              };
+            });
+          }
+        }
+      });
+});
 
+
+
+//highlight menu item on scroll
+
+$(document).ready(function(){
+  
+    'use strict';
+    
+    $(window).scroll(function(){
+                     'use strict';
+                    
+                     $("section").each(function(){
+                           
+                            'use strict';
+                            
+                            var bb= $(this).attr("id"); // ABOUT, CONTACT, DOWNLOAD
+                           // console.log(bb);
+                            
+                            var height=$(this).outerHeight();
+                            //console.log(height);    
+                            var grttop =$(this).offset().top - 350;
+                           // console.log(grttop);
+        
+                            if($(window).scrollTop()>grttop && $(window).scrollTop() < (grttop + height)){
+                                
+                                //$(".navbar-nav li a[href='#" +bb + "']") ///a[href='#ABOUT']
+                               
+                                $(".navbar-nav li a[href='#" + bb + "']").parent().addClass("active");
+                            } else{
+                                $(".navbar-nav li a[href='#" + bb + "']").parent().removeClass("active");
+                            }
+        
+                                
+                            });
+                     });
+               
+               
+    
+});
+
+//add auto padding to header
+
+$(document).ready(function(){
+    
+    'use strict';
+    
+    setInterval(function(){
+        
+        'use strict';
+        
+        var wiindowHeight = $(window).height();
+        
+        var containerHeight = $(".header-overlay .container").height();
+        
+        var padTop= wiindowHeight - containerHeight;
+        
+        $(".header-overlay .container").css({
+            'padding-top': Math.round(padTop/3) + 'px',
+            'padding-bottom': Math.round(padTop/3) + 'px'
+        })
+        
+    } ,10)
+});
 
 
 
